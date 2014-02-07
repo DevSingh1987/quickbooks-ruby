ca2bc5c930a694bf27b8716867ad2c22bfbb980c
Steven Chau <steven.p.chau@gmail.com>
2014-02-07 13:07:18 -0800

    Build JournalEntryLineItem

--- lib/quickbooks/model/line.rb
+++ lib/quickbooks/model/line.rb
@@ -5,6 +5,7 @@ module Quickbooks
       SALES_LINE_ITEM_DETAIL = 'SalesItemLineDetail'
       SUB_TOTAL_LINE_DETAIL = 'SubTotalLineDetail'
       PAYMENT_LINE_DETAIL = 'PaymentLineDetail'
+      JOURNAL_ENTRY_LINE_DETAIL = 'JournalEntryLineDetail'
 
       xml_accessor :id, :from => 'Id', :as => Integer
       xml_accessor :line_num, :from => 'LineNum', :as => Integer
@@ -16,12 +17,20 @@ module Quickbooks
       xml_accessor :sales_item_line_detail, :from => 'SalesItemLineDetail', :as => SalesItemLineDetail
       xml_accessor :sub_total_line_detail, :from => 'SubTotalLineDetail', :as => SubTotalLineDetail
       xml_accessor :payment_line_detail, :from => 'PaymentLineDetail', :as => PaymentLineDetail
+      xml_accessor :journal_entry_line_detail, :from => 'JournalEntryLineDetail', :as => JournalEntryLineDetail
 
       def sales_item!
         self.detail_type = SALES_LINE_ITEM_DETAIL
         self.sales_item_line_detail = SalesItemLineDetail.new
 
         yield self.sales_item_line_detail if block_given?
+      end
+
+      def journal_item!
+        self.detail_type = JOURNAL_ENTRY_LINE_DETAIL
+        self.journal_entry_line_detail = JournalEntryLineDetail.new
+
+        yield self.journal_entry_line_detail if block_given?
       end
     end
   end
